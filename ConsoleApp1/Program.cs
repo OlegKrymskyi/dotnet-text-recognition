0b2598778ed943df01457a1dba5a1c9b81766b23
Oleg Krymskyi <krymskyi.oleg@gmail.com>
2022-03-16 13:39:11 +0200

    image loading

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ using System.Runtime.InteropServices;
 using Emgu.CV.Structure;
 using Numpy.Models;
 using SkiaSharp;
+using System.Drawing;
 
 namespace ConsoleApp1
 {
@@ -21,8 +22,9 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
 
-            using var fs = new SKFileStream("assets/1.jpg");
-            using var img = SKBitmap.Decode(fs);
+            //using var img = (Bitmap)Bitmap.FromFile("assets/1.jpg");
+
+            //var imgData = img.ToNDArray(flat: false, copy: false);
 
             MLContext mlContext = new MLContext();
 
@@ -32,7 +34,8 @@ namespace ConsoleApp1
                 .LoadImages(
                     outputColumnName: "image", imageFolder: "assets",
                     inputColumnName: nameof(ImageNetData.ImagePath))
-                .Append(mlContext.Transforms.ResizeImages(resizing: ImageResizingEstimator.ResizingKind.Fill, outputColumnName: "image", imageWidth: 1280, imageHeight: 1184, inputColumnName: "image"))
+                .Append(mlContext.Transforms.ResizeImages(resizing: ImageResizingEstimator.ResizingKind.Fill, 
+                    outputColumnName: "image", imageWidth: 1280, imageHeight: 1184, inputColumnName: "image"))
                 .Append(mlContext.Transforms.ExtractPixels(
                     outputColumnName: "image"))
                 .Append(mlContext.Transforms.ApplyOnnxModel(
@@ -47,6 +50,10 @@ namespace ConsoleApp1
             var predictEngine = mlContext.Model.CreatePredictionEngine<ImageNetData, ImageNetPrediction>(mlNetModel);
 
             var result = predictEngine.Predict(new ImageNetData() { ImagePath = "1.jpg", Label= "1" });
+
+            var textmapShape = np.array(result.textmap).shape;
+            var img_h = textmapShape[0];
+            var img_w = textmapShape[1];
 
             Mat scoreText = Mat.Ones(1, result.textmap.Length, DepthType.Cv32F, 1);
             scoreText.SetTo<float>(result.textmap);
@@ -81,16 +88,146 @@ namespace ConsoleApp1
                     continue;
                 }
 
+                var textMapArr = new List<float>();
                 var labelsTemp = new List<bool>();
+                int i = 0;
                 foreach (int label in labels.GetData())
                 {
                     labelsTemp.Add(label == k);
+
+                    if (label == k)
+                    {
+                        textMapArr.Add(result.textmap[i]);
+                    }
+
+                    i++;
                 }
+
+
                 // thresholding
+                var textmapArr = new NDarray<float>(result.textmap);
+                //textmapArr[labelsTemp];
                 //if np.max(textmap[labels == k]) < text_threshold: continue
 
-                //var segmap = np.zeros(new Shape(result.textmap), dtype: np.uint8);
+                if ((float)np.max(new NDarray<float>(textMapArr.ToArray())) < 0.7)
+                {
+                    continue;
+                }
+
+                //var segmap = np.zeros((new NDarray<float>(result.textmap)).shape, dtype: np.uint8);
+                //i = 0;
+                //foreach (int label in labels.GetData())
+                //{
+                //    if (label == k)
+                //    {
+                //        segmap[i] = (NDarray)255;
+                //    }
+
+                //    i++;
+                //}
+
+                var x = stats[k, (int)ConnectedComponentsTypes.Left];
+                var y = stats[k, (int)ConnectedComponentsTypes.Top];
+                var w = stats[k, (int)ConnectedComponentsTypes.Width];
+                var h = stats[k, (int)ConnectedComponentsTypes.Height];
+
+                var niter = (int)(Math.Sqrt(size * Math.Min(w, h) / (w * h)) * 2);
+                var sx = x - niter;
+                var ex = x + w + niter + 1;
+                var sy = y - niter;
+                var ey = y + h + niter + 1;
+
+                if (sx < 0) sx = 0;
+                if (sy < 0) sy = 0;
+                if (ex >= img_w) ex = img_w;
+                if (ey >= img_h) ey = img_h;
             }
+        }
+
+        private static Tuple<NDarray, float> resize_aspect_ratio(NDarray img, float square_size, Inter interpolation, float mag_ratio = 1)
+        {
+            var height = img.shape[0];
+            var width = img.shape[1];
+            var channel = img.shape[2];
+
+            // magnify image size
+            var target_size = mag_ratio * Math.Max(height, width);
+
+            // set original image size
+            if (target_size > square_size)
+            {
+                target_size = square_size;
+            }
+
+            var ratio = target_size / Math.Max(height, width);
+
+            var target_h = (int)(height * ratio);
+            var target_w = (int)(width * ratio);
+
+            var imgMat = new Mat();
+            CvInvoke.Resize(ToMatImage(img), imgMat, new Size(target_w, target_h), interpolation: interpolation);
+
+            //# make canvas and paste image
+            var target_h32 = target_h;
+            var target_w32 = target_w;
+
+            if (target_h % 32 != 0) {
+                target_h32 = target_h + (32 - target_h % 32);
+            }
+
+            if (target_w % 32 != 0)
+            {
+                target_w32 = target_w + (32 - target_w % 32);
+            }
+
+            var resized = ToImageNDarray(imgMat, target_w32, target_h32, channel);
+
+            return new Tuple<NDarray, float>(resized, ratio);
+        }
+
+        private static NDarray normalizeMeanVariance(NDarray in_img, NDarray mean, NDarray variance)
+        {
+            // should be RGB order
+            var img = in_img.copy().astype(np.float32);
+
+            img -= np.array(new NDarray[] { mean[0] * 255.0, mean[1] * 255.0, mean[2] * 255.0 }, dtype: np.float32);
+            img /= np.array(new NDarray[] { variance[0] * 255.0, variance[1] * 255.0, variance[2] * 255.0 }, dtype: np.float32);
+            
+            return img;
+        }
+
+        private static NDarray cvt2HeatmapImg(NDarray img)
+        {
+            img = (np.clip(img, (NDarray)0, (NDarray)1) * 255).astype(np.uint8);
+            var matImg = new Mat();
+            CvInvoke.ApplyColorMap(ToMatImage(img), matImg, ColorMapType.Jet);
+            return ToImageNDarray(matImg, matImg.Cols, matImg.Rows, matImg.NumberOfChannels);
+        }
+
+        private static Mat ToMatImage(NDarray npArrary)
+        {
+            var result = Mat.Eye(npArrary.shape[0], npArrary.shape[1], DepthType.Cv32F, npArrary.shape[2]);
+            result.SetTo<float>(npArrary.GetData<float>());
+            return result;
+        }
+
+        private static NDarray ToImageNDarray(Mat mat, int width, int height, int channels)
+        {
+            var resized = np.zeros((height, width, mat.NumberOfChannels), dtype: np.float32);
+
+            var data = new float[height * width * mat.NumberOfChannels];
+            mat.CopyTo<float>(data);
+            for (var r = 0; r < height; r++)
+            {
+                for (var c = 0; c < width; c++)
+                {
+                    for (var i = 0; i < channels; i++)
+                    {
+                        resized[r, c, i] = (NDarray)data[r + c + i];
+                    }
+                }
+            }
+            return resized;
         }
 
         private static float[] ToFloatVector(Mat mat)
