8769813fb987d700609f71164c08851b38b96048
Oleg Krymskyi <krymskyi.oleg@gmail.com>
2022-03-24 19:49:29 +0200

    scaling

--- TextDetection.Console.App/Program.cs
+++ TextDetection.Console.App/Program.cs
@@ -18,6 +18,9 @@ namespace TextDetection.App
 
             var size = new Size(1280, 1280);
             using var imageMat = new Mat("assets/1.jpg", loadType: ImreadModes.Color);
+            var ratioX = imageMat.Width / (float)size.Width;
+            var ratioY = imageMat.Height / (float)size.Height;
+
             using var resizedMat = new Mat();
             CvInvoke.Resize(imageMat, resizedMat, size);
 
@@ -42,11 +45,12 @@ namespace TextDetection.App
             }
 
             using var grayImage = new Mat();
-            CvInvoke.CvtColor(resizedMat, grayImage, ColorConversion.Bgr2Gray);
+            CvInvoke.CvtColor(imageMat, grayImage, ColorConversion.Bgr2Gray);
             foreach (var idx in result.Boxes.Keys)
             {
-                CvInvoke.Polylines(resizedMat, result.Boxes[idx].Select(x => new Point((int)(x.X), (int)(x.Y))).ToArray(), true, new MCvScalar(255, 0, 0), thickness: 5);
-                var text = recognizer.Recognize(grayImage, result.Boxes[idx]);
+                var points = result.Boxes[idx].Select(x => new PointF(x.X* ratioX, x.Y * ratioY)).ToArray();
+                CvInvoke.Polylines(imageMat, points.Select(pt => new Point((int)(pt.X), (int)(pt.Y))).ToArray(), true, new MCvScalar(255, 0, 0), thickness: 5);
+                var text = recognizer.Recognize(grayImage, points);
 
                 if (!string.IsNullOrWhiteSpace(text))
                 {
@@ -54,7 +58,7 @@ namespace TextDetection.App
                 }
             }
 
-            CvInvoke.Imwrite("result_1.jpg", resizedMat);
+            CvInvoke.Imwrite("result_1.jpg", imageMat);
         }
     }
 }
